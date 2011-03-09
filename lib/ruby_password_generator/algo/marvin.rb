d903a420e7afbfb6452deffa60092b5f0f4ed737
Aziz Light <aziiz.light+github@gmail.com>
2011-03-09 17:42:43 +0100

    Finalized basic password generation

--- lib/ruby_password_generator/algo/marvin.rb
+++ lib/ruby_password_generator/algo/marvin.rb
@@ -8,22 +8,11 @@ module RubyPasswordGenrator
     M_SPECCIAL_CHARACTERS = "!@#()_-+=[]{} ".split("")
 
     def initialize
-      
     end
 
     def generate
-      " " * 42
-    end
-
-    def identity
-      
-    end
-
-    private
-
-    def get_random_character
-      char = RubyPasswordGenrator::Marvin.const_get(:M_LOWERCAS_ELETTERS)
-      char
+      password = (M_LOWERCAS_LETTERS + M_UPPERCASE_LETTERS).shuffle.join[0...42]
+      password
     end
 
   end
--- spec/algo/marvin_spec.rb
+++ spec/algo/marvin_spec.rb
@@ -37,7 +37,38 @@ describe "Marvin" do
       @generator.generate.is_a?(String).must_be :==, true, "The generate method should return a string."
       @generator.generate.length.must_be :==, 42, "The generate method should generate a 42 characters long string."
     end
+
+    it "should not generate passwords with duplicate characters" do
+      valid = true
+      password = @generator.generate
+      password.split("").each do |char|
+        if password.count(char) > 1
+          valid = false
+          break
+        end
+      end
+
+      valid.must_be :==, true, "There shouldn't be repeating chars in the generated password"
+    end
+
+    it "should not have the same letter repeated twice in a row in different case" do
+      valid = true
+      for i in 1...42
+        password = @generator.generate.split("")
+        password.each_with_index do |char, index|
+          if password[index].downcase == password[index + 1].downcase
+            valid = false
+            break
+          end
+        end
+
+        break unless valid
+      end
+
+      valid.must_be :==, true, "The same character can't be repeated twice in a row with a different case"
+    end
     
+
     
   end
 end

6b6adad3957f77f0e0b5718c2d367141d7423116
Aziz Light <aziiz.light+github@gmail.com>
2011-03-14 12:17:00 +0100

    necessary requires

--- lib/ruby_password_generator.rb
+++ lib/ruby_password_generator.rb
@@ -1,5 +1,9 @@
+require "sinatra/base"
+require "erb"
+
 require_relative "ruby_password_generator/ruby_password_generator"
 require_relative "ruby_password_generator/password_generator"
+require_relative "ruby_password_generator/webapp"
 
 # DEBUG
 require "pp"

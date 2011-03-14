b5dbe1a2c4275b7777a05c283c1f6bbd0deba148
Aziz Light <aziiz.light+github@gmail.com>
2011-03-14 12:57:34 +0100

    Finished the web app

--- lib/ruby_password_generator/webapp.rb
+++ lib/ruby_password_generator/webapp.rb
@@ -8,5 +8,19 @@ module RPG
     get '/' do
       erb "Hello World"
     end
+
+    get /\/([\w\d]{2,})\/([\w\d]+)/i do |algorithm, length|
+      begin
+        erb RPG::PasswordGenerator.new(algorithm.downcase.capitalize.to_sym).generate(length)
+      rescue
+        erb "A problem occured while processing the arguments you passed to the #{algorithm.downcase.capitalize} algorithm..."
+      end
+    end
+
+    get /\/([\w\d]{2,})/i do |algorithm|
+      erb RPG::PasswordGenerator.new(algorithm.downcase.capitalize.to_sym).generate
+    end
+
+    
   end
 end
